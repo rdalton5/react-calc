3c10f5d4452395036f4a7e69a6b222a652ec2195
Robert Dalton <rdalton5@kent.edu>
2019-11-28 13:27:11 -0500

    created a backspace button

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -47,6 +47,7 @@ export default class Calculator extends React.Component {
 		this.chooseOperator = this.chooseOperator.bind(this);
 		this.calculate = this.calculate.bind(this);
 		this.clear = this.clear.bind(this);
+		this.backspace = this.backspace.bind(this);
 	}
 
 	render() {
@@ -57,6 +58,7 @@ export default class Calculator extends React.Component {
 				<div>
 					{makeButtonFrom("=", this.calculate)}
 					{makeButtonFrom("CLEAR", this.clear)}
+					{makeButtonFrom("<", this.backspace)}
 				</div>
 				<NumberPad clickHandler={this.chooseNumber} />
 				<OperatorButtons clickHandler={this.chooseOperator} />
@@ -90,6 +92,24 @@ export default class Calculator extends React.Component {
 			rightOperand: 0,
 			operation: undefined
 		});
+	}
+
+	backspace() {
+		if(this.state.operation === undefined) {
+			this.setState({leftOperand: removeLastDigitEntered(this.state.leftOperand)});
+		} else {
+			this.setState({rightOperand: removeLastDigitEntered(this.state.leftOperand)});
+		}
+
+		function removeLastDigitEntered(number){
+			const stringifiedNumber = String(number);
+			const shorterNumber = stringifiedNumber.substring(0, stringifiedNumber.length - 1);
+			if(shorterNumber.length === 0){
+				return 0;
+			} else {
+				return Number(shorterNumber);
+			}
+		}
 	}
 
 	calculate() {
